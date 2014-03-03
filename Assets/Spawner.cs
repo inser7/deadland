26c14d5631db437ed91dabcd90310073564d3e6b
PavelDevelop <paveldsirius@gmail.com>
2014-03-03 23:57:21 +0400

    Добавил генератор мобов, привел в порядок и переименовал скрипт ZombieWalk.cs Все работает проверяйте.

---
+++ Assets/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    #region Fields
+    public float spawnTime = 5f;		// The amount of time between each spawn.
+    public float spawnDelay = 3f;		// The amount of time before spawning starts.
+    public GameObject[] zombies;		// Array of enemy prefabs.
+    #endregion
+
+    #region Start
+    void Start () {
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
+	}
+    #endregion
+
+    #region Update is called once per frame
+    void Spawn () {
+
+        // Instantiate a random enemy.
+        var enemyIndex = Random.Range(0, zombies.Length);
+        Instantiate(zombies[enemyIndex], transform.position, transform.rotation);
+
+        // Play the spawning effect from all of the particle systems.
+        foreach (var p in GetComponentsInChildren<ParticleSystem>())
+        {
+            p.Play();
+        }
+    }
+    #endregion
+
+}
