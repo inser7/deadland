f9ea6a8765497cee608e98178d8c7c7849fec439
Sergey Mozharovskiy <sergey.mozharovskiy@gmail.com>
2014-03-05 12:39:05 +0400

    Убрал лишние строчки. типа отпимизация

--- Assets/Scripts/HeroControllerScript.cs
+++ Assets/Scripts/HeroControllerScript.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HeroControllerScript : MonoBehaviour {
+public class HeroControllerScript : MonoBehaviour 
+{
 
-	//переменная для установки макс. скорости персонажаssss
+	//переменная для установки макс. скорости персонажа
 	public float maxSpeed = 10.0f; 
 	public float turnAngle = 5.0f; 
 	public float currentZRotation = 0.0f; 
@@ -25,19 +26,18 @@ public class HeroControllerScript : MonoBehaviour {
 	{
 		Vector2 moveDirection 	= new Vector2 ( Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") );
 
-		if (Mathf.Abs (moveDirection.y) > 0.5f) 
-		{ //если двигаемся
+		if (Mathf.Abs (moveDirection.y) > 0.0f) 
+		{ //если двигаемся вперед
 			anim.SetFloat ("Speed", Mathf.Abs (moveDirection.y));		
 			rigidbody2D.velocity = forwardDirection * maxSpeed * moveDirection.y;
-		} else
-			rigidbody2D.velocity = new Vector2 (0.0f, 0.0f );
+		}
 					
-		if( Mathf.Abs( moveDirection.x ) > 0.2f) //если поворачиваем
+		if( Mathf.Abs( moveDirection.x ) > 0.0f) //если поворачиваем
 		{
 			anim.SetFloat("Speed", Mathf.Abs(moveDirection.x));
 
 			currentZRotation = rigidbody2D.transform.rotation.eulerAngles.z;
-			if( moveDirection.x > 0 ) currentZRotation -= turnAngle; else currentZRotation += turnAngle;
+			currentZRotation -= turnAngle * moveDirection.x;
 
 			float newX 	= forwardDirection.x * Mathf.Cos( Mathf.Deg2Rad * ( currentZRotation - rigidbody2D.transform.rotation.eulerAngles.z ) ) 
 				- forwardDirection.y * Mathf.Sin( Mathf.Deg2Rad * ( currentZRotation - rigidbody2D.transform.rotation.eulerAngles.z ) );// +rigidbody2D.transform.position.x;
@@ -45,12 +45,9 @@ public class HeroControllerScript : MonoBehaviour {
 				+ forwardDirection.y * Mathf.Cos( Mathf.Deg2Rad * ( currentZRotation - rigidbody2D.transform.rotation.eulerAngles.z ) );// +rigidbody2D.transform.position.y;
 
 			forwardDirection = new Vector2( newX, newY ) ;
-
 			forwardDirection.Normalize();
+
 			rigidbody2D.transform.rotation= Quaternion.Euler(0.0f, 0.0f,  currentZRotation );
 		}
-
-
 	}
-
 }
