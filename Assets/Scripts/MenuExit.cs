cb2ac2646481f30c998a14038cc06280f79b7770
Igor Pellinen <igronus@gmail.com>
2014-03-06 12:25:42 +0400

    Added menu.

---
+++ Assets/Scripts/MenuExit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuExit : MonoBehaviour {
+
+	void OnMouseDown(){
+		//Debug.Log(this);
+		Application.Quit ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MenuPlay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPlay : MonoBehaviour {
+	
+	void OnMouseDown(){
+		//	Debug.Log(this);
+		Application.LoadLevel("MainScene.unity.REMOTE");
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
