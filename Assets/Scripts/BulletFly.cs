487172987cdc1e57e56916d5464118602773408b
Sergey Mozharovskiy <sergey.mozharovskiy@gmail.com>
2014-03-06 12:59:40 +0400

    Стреляемс уже. только для пулек время жизни ограничить надо

---
+++ Assets/Scripts/BulletFly.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletFly : MonoBehaviour {
+
+	public float maxSpeed = 20.0f; 
+	public Vector2 forwardDirection = new Vector2 (0.0f, 1.0f);
+	public float timeToDie = 3.0f;
+	// Use this for initialization
+	void Start () 
+	{
+		float newX 	= forwardDirection.x * Mathf.Cos( Mathf.Deg2Rad * ( transform.rotation.eulerAngles.z ) ) 
+					- forwardDirection.y * Mathf.Sin( Mathf.Deg2Rad * ( transform.rotation.eulerAngles.z ) );
+		float newY 	= forwardDirection.x * Mathf.Sin( Mathf.Deg2Rad * ( transform.rotation.eulerAngles.z ) ) 
+					+ forwardDirection.y * Mathf.Cos( Mathf.Deg2Rad * ( transform.rotation.eulerAngles.z ) );
+		
+		forwardDirection = new Vector2( newX, newY ) ;
+		forwardDirection.Normalize();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+		//if( timeToDie <= 0.0f ) Destroy(
+		rigidbody2D.velocity = forwardDirection * maxSpeed;
+	}
+}
--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -3,17 +3,24 @@ using System.Collections;
 
 public class WeaponController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+
+	public GameObject bullet;
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+
 		Vector3 lookDirection = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
 		var currentZRotation = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90;
 		transform.rotation= Quaternion.Euler(0.0f, 0.0f,  currentZRotation );
+		if (Input.GetButton ("Fire1")) 
+		{
+			var cloneBullet = Instantiate( bullet, transform.position, transform.rotation);
+
+		}
 	}
 }
