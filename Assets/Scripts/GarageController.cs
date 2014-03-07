f4d2085926e24dd9cb03b70e45739049e5cd9294
Sergey Mozharovskiy <sergey.mozharovskiy@gmail.com>
2014-03-07 18:18:26 +0400

    Сделал простую Гаражную Сцену. Трактор крутится, мышкой тоже можно помочь покрутить при зажимании ЛКМ

---
+++ Assets/Scripts/GarageController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GarageController : MonoBehaviour {
+	
+	//private Vector3 tracktorTransform;
+	private Transform tracktorTransform;
+	public float angle = 5.0f;
+	// Use this for initialization
+	void Start () 
+	{
+		tracktorTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Vector3 clickPosition;// = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
+		tracktorTransform.RotateAround (tracktorTransform.position, new Vector3 (0.0f, 1.0f, 0.0f), angle * Mathf.Deg2Rad);
+
+		if (Input.GetButton ("Fire1")) 
+		{
+			Vector3 clickPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
+			tracktorTransform.RotateAround (tracktorTransform.position, new Vector3 (0.0f, 1.0f, 0.0f), angle * Mathf.Deg2Rad  * -50 * Input.GetAxis("Mouse X"));
+
+		}
+
+	}
+}
---
+++ Assets/Scripts/GarageMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GarageMenu : MonoBehaviour {
+
+	
+	void OnMouseDown(){
+		//	Debug.Log(this);
+		Application.LoadLevel("2dmenu");
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
