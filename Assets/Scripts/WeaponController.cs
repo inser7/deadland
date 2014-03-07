2ad9fefdbcb9702607081e3f2c20c3ee27e759e3
Sergey Mozharovskiy <sergey.mozharovskiy@gmail.com>
2014-03-07 23:11:03 +0400

    Добавил свойство скорострельности к пулемету.

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -3,25 +3,38 @@ using System.Collections;
 
 public class WeaponController : MonoBehaviour {
 
+	#region Fields
+		public GameObject bullet;
+		public float rateOfFire = 0.1f;
+		private float timeToFire;
+		private Transform weaponTransform;
+	#endregion
 
-	public GameObject bullet;
+	#region void Start ()
 	void Start () 
 	{
-	
+		timeToFire = Time.time + rateOfFire;
+		weaponTransform = transform;
 	}
-	
+	#endregion
+
+	#region void Update ()
 	// Update is called once per frame
 	void Update () 
 	{
 
-		Vector3 lookDirection = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
+		Vector3 lookDirection = Camera.main.ScreenToWorldPoint (Input.mousePosition) - weaponTransform.position;
 		var currentZRotation = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90;
-		transform.rotation= Quaternion.Euler(0.0f, 0.0f,  currentZRotation );
+		weaponTransform.rotation= Quaternion.Euler(0.0f, 0.0f,  currentZRotation );
+		
+		if(Time.time > timeToFire)
 		if (Input.GetButton ("Fire1")) 
-		{
-			var cloneBullet = Instantiate( bullet, transform.position, transform.rotation);
+		{ 
+			timeToFire = Time.time + rateOfFire;
+			var cloneBullet  = Instantiate( bullet, weaponTransform.position, weaponTransform.rotation);
 
 			Destroy(cloneBullet, 1);
 		}
 	}
+	#endregion
 }
