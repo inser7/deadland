fd3ddfc7fabcd3b047e3e602e51a7c3c23106f84
Sergey Mozharovskiy <sergey.mozharovskiy@gmail.com>
2014-03-09 22:46:42 +0400

    Добавил в гараж окно выбора оружия. примерно

--- Assets/Scripts/GarageController.cs
+++ Assets/Scripts/GarageController.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿using UnityEngine; 
 using System.Collections;
 
 public class GarageController : MonoBehaviour {
@@ -6,6 +6,10 @@ public class GarageController : MonoBehaviour {
 	#region Fields
 	private Transform tracktorTransform;
 	public float angle = 5.0f;
+	public GameObject[] weapons;
+	public bool isTopWeaponExist = true;
+
+	//private bool isTopWeaponCreate = true;
 	#endregion
 	// Use this for initialization
 	#region void Start () 
@@ -23,6 +27,18 @@ public class GarageController : MonoBehaviour {
 		if (Input.GetButton ("Fire1")) 
 		{
 			tracktorTransform.RotateAround (tracktorTransform.position, new Vector3 (0.0f, 1.0f, 0.0f), angle * Mathf.Deg2Rad  * -50 * Input.GetAxis("Mouse X"));
+		}
+
+		//if( isTopWeaponCreate
+		if (!isTopWeaponExist) 
+		{
+			isTopWeaponExist = false;
+			Vector3 pos = tracktorTransform.position;
+			Quaternion rot = tracktorTransform.rotation;
+			rot.eulerAngles = new Vector3( -90.0f ,rot.eulerAngles.x,tracktorTransform.rotation.eulerAngles.z); 
+			pos.y += 1.0f;
+			var cloneTopWeapon  = Instantiate( weapons[0], pos, rot);
+			//gameObject.
 		}
 	}
 	#endregion
---
+++ Assets/Scripts/garageInventoryController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class garageInventoryController : MonoBehaviour {
+	
+	#region Fields
+	private Transform slotTransform;
+	public float angle = 25.0f;
+	public GameObject[] gameObjs;
+	#endregion
+	// Use this for initialization
+	#region void Start () 
+	void Start () 
+	{
+		slotTransform = transform;
+	}
+	#endregion
+	
+	// Update is called once per frame
+	#region void Update ()
+	void Update () 
+	{
+		if (gameObject.tag != "invButtons")
+			slotTransform.RotateAround (slotTransform.position, new Vector3 (0.0f, 1.0f, 0.0f), angle * Mathf.Deg2Rad);
+
+	}
+	#endregion
+	
+	#region OnMouseDown()
+	void OnMouseDown()
+	{
+		if (gameObject.tag == "invButtons")
+		{
+			Vector3 slotPosition = gameObjs[0].transform.position;
+			if( slotPosition.y == 8.0f ) slotPosition.y = 2.5f; else slotPosition.y = 8.0f;
+			
+			gameObjs[0].transform.position = slotPosition;
+
+			slotPosition = gameObjs[1].transform.position;
+			if( slotPosition.y == 8.0f ) slotPosition.y = 2.5f; else slotPosition.y = 8.0f;
+			
+			gameObjs[1].transform.position = slotPosition;
+
+
+			#region buttons
+			if( gameObject.name == "Up Button" )
+			{
+
+			}
+
+			if( gameObject.name == "Down Button" )
+			{
+
+			}
+			#endregion
+		}
+	}
+	#endregion
+}
