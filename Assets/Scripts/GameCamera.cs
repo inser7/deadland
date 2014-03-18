979141ee78b1d2159580ff8d2a1bfb02251222b7
ins7 <inser7@gmail.com>
2014-03-18 15:37:17 +0400

    Revert "пробный вариант не жесткой привязки камеры"
    This reverts commit 7af6c0e7cc917590207bd3f68dc5bf8de43aeba5.

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -7,19 +7,9 @@ public class GameCamera : MonoBehaviour
     #region Fields
 
     public Transform target;
-	public float distanceToTarget = 5.0f;
     private float trackSpeed = 10;
-	private Transform cameraTransform;
+
     #endregion
-
-	#region void Start () 
-	void Start () 
-	{
-		cameraTransform = transform;
-		
-
-	}
-	#endregion
 
     #region Set Target
 
@@ -36,16 +26,11 @@ public class GameCamera : MonoBehaviour
         if (!target) 
             return;
 
-		if( Vector3.Magnitude( cameraTransform.position - target.position ) > distanceToTarget )
-		{
-			cameraTransform.position = Vector3.Lerp( cameraTransform.position, target.position, Time.deltaTime * 2.0f );
-		}
-       /* var v = transform.position;
+        var v = transform.position;
         v.x = target.position.x;
         v.y = target.position.y;
 
         transform.position = Vector3.MoveTowards(transform.position, v, trackSpeed * Time.deltaTime);
-		*/
-	}
+    }
     #endregion
 }
