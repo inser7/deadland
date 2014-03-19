55fc2499f6027bf2579595bdd4cb759f997397e5
ins7 <inser7@gmail.com>
2014-03-19 22:40:16 +0400

    #11 Тикет. Тестовое меню на Ngui

---
+++ Assets/Scripts/Menu/menuV2/Control.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control : MonoBehaviour {
+
+	// Use this for initialization
+	void Newgame () {
+        Application.LoadLevel("MainScene.unity.REMOTE");
+	
+	}
+	
+	// Update is called once per frame
+	void Garage () {
+        Application.LoadLevel("garage3D");
+	}
+}
