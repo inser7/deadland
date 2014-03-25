305098e4ec2b631b004a98a2acc496cd14154d59
ins7 <inser7@gmail.com>
2014-03-25 16:34:59 +0400

    игрался с прогрузкой уровня

--- Assets/Scripts/Menu/menuV2/Control.cs
+++ Assets/Scripts/Menu/menuV2/Control.cs
@@ -3,14 +3,84 @@ using System.Collections;
 
 public class Control : MonoBehaviour {
 
+
+	private AsyncOperation Checker; //Загрузка уровня
+	public UISlider  LoadSlider;
+	private float loading_progress = 0f;
+	private int Round_load;
+
+
+	private string LeveName;
+	private GUIStyle SKIN;
+	private bool Menu = true;
 	// Use this for initialization
+	/*
+	void start(){
+		LoadSlider.sliderValue = 0f;
+	}*/
 	void Newgame () {
-        Application.LoadLevel("MainScene.unity.REMOTE");
-	
+        //Application.LoadLevel("MainScene.unity.REMOTE");
+		//Asop = Application.LoadLevelAsync ("MainScene.unity.REMOTE");
+		//Asop.allowSceneActivation = false;
+		/*while (!Asop.isDone) 
+		{
+			LoadSlider.sliderValue =  (int)(	*100f);
+			//yield return null;
+		} */
 	}
-	
+
+
+	void OnGUI(){
+		if(Menu){
+			if(Input.GetKeyDown ("space")){
+				
+				Checker=Application.LoadLevelAsync("MainScene.unity.REMOTE");
+				
+				Checker.allowSceneActivation=false;
+				Menu=false;
+				
+			}
+		}
+		
+		if(!Checker.isDone){
+			if(Input.GetButton ("Fire1")){
+				Checker.allowSceneActivation=true;
+			}
+		}
+		
+		
+		if(Checker !=null){
+			GUI.Box(new Rect(0, Screen.height-50,Checker.progress*Screen.width*100,40),"");
+			
+		}
+		
+		
+		
+	}
+
+	//А тут я пытался из примера с запуском подпрограммы
+	/*void Update()
+	{
+		if (Input.GetKeyDown ("space")) {
+			StartCoroutine(DisplayLoadingScreen("MainScene.unity.REMOTE"));
+		}
+	}
+
+	IEnumerator DisplayLoadingScreen(string Level)
+	{	
+		//LoadSlider.sliderValue = (Round_load);
+		AsyncOperation async = Application.LoadLevelAdditiveAsync (Level);
+		while (!async.isDone) {
+			//Round_load = (int)(async.progress*100);
+			LoadSlider.sliderValue = (async.progress);
+			yield return null;
+		}
+	}
+*/
+
 	// Update is called once per frame
 	void Garage () {
-        Application.LoadLevel("garage3D");
+		//Asop.allowSceneActivation = true;
+        //Application.LoadLevel("garage3D");
 	}
 }
