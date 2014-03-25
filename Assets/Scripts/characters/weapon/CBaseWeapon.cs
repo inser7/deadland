c3636c3973831202ea61a4f46d6b9d787b6dac59
Sergey Mozharovskiy <sergey.mozharovskiy@gmail.com>
2014-03-25 11:01:13 +0400

    Начал работать над базовым классом оружия. мелкие косяки поправил.

---
+++ Assets/Scripts/characters/weapon/CBaseWeapon.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CBaseWeapon : MonoBehaviour
+{
+    #region Fields
+
+		// объект пули для выстрела
+		public GameObject bullet;
+		//будет ли оружие поворачиваться за мышкой?
+		public bool fixedAngle = true;
+		//основное или альтернативное
+		public bool isAlternative = false;
+
+
+		//объект для звука выстрела/удара
+		public GameObject gunshot;
+
+    #endregion
+
+	// Use this for initialization
+	#region void Start ()
+	void Start ()
+	{
+
+	}
+    #endregion
+
+	// Update is called once per frame
+	#region void Update ()
+	void Update ()
+	{
+
+	}
+	#endregion
+}
