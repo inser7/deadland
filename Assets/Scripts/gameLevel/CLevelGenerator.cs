2b11596acfcccc89b5d84b2308268ad8192be2fb
Sergey Mozharovskiy <sergey.mozharovskiy@gmail.com>
2014-03-28 09:59:35 +0400

    Проект приведен в порядок. Лишнее удалено. префабы по папками разложил

---
+++ Assets/Scripts/gameLevel/CLevelGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CLevelGenerator : MonoBehaviour
+{
+
+    #region fields
+    public GameObject tile;
+    //кол-во тайлов в ширину
+    public int mapWidth  = 4;
+    //кол-во тайлов в высоту
+    public int mapHeight = 4;
+    #endregion
+
+//(new GameObject("AutoFade")).AddComponent<AutoFade>();
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
